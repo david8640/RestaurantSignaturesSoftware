f286eafe0fcada1b965edc426cd2bd66dfa53cae
Andrew <aassa013@uottawa.ca>
2013-10-24 19:23:44 -0400

    Updated database scripts to make way for login functionality.

--- Database scripts/1-restaurantManagementSoftware.sql
+++ Database scripts/1-restaurantManagementSoftware.sql
@@ -19,6 +19,8 @@ USE `restaurantManagementSoftware`;
 -- Drop tables
 -- ---------------------------------------------------------------------------
 DROP TABLE IF EXISTS `supplier`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `login_attempts`;
 
 -- ---------------------------------------------------------------------------
 -- Table supplier
@@ -30,6 +32,33 @@ CREATE TABLE IF NOT EXISTS `supplier` (
   `fax_number` INT(11),
   PRIMARY KEY (`id_supplier`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5;
+
+-- ---------------------------------------------------------------------------
+-- Table users
+-- ---------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `first_name` varchar(75) NOT NULL,
+  `last_name` varchar(75) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` char(128) NOT NULL,
+  `salt` char(128) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+-- ---------------------------------------------------------------------------
+-- Table login_attempts
+-- ---------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `id_user` int(11) NOT NULL,
+  `timeOfAttempt` varchar(30) NOT NULL,
+  `ip_address` varchar(30) DEFAULT 'n/a',
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `login_attempts_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- Database scripts/2-restaurantManagementSoftware_sp.sql
+++ Database scripts/2-restaurantManagementSoftware_sp.sql
@@ -14,6 +14,29 @@ AS
 GO
 
 -- -----------------------------------------------------
+-- View `v_getUsers`
+-- -----------------------------------------------------
+DROP VIEW IF EXISTS `v_getUsers`
+GO
+CREATE VIEW v_getUsers
+AS
+	SELECT id_user, username, first_name, last_name, email, password, salt
+	FROM users;
+GO
+
+-- -----------------------------------------------------
+-- View `v_getLoginAttempts`
+-- -----------------------------------------------------
+DROP VIEW IF EXISTS `v_getLoginAttempts`
+GO
+CREATE VIEW v_getLoginAttempts
+AS
+	SELECT id_user, timeOfAttempt, ip_address
+	FROM login_attempts;
+GO
+
+
+-- -----------------------------------------------------
 -- Stored Procedure `sp_getSupplier`
 -- -----------------------------------------------------
 DROP PROCEDURE IF EXISTS `sp_getSupplier`
@@ -67,4 +90,65 @@ BEGIN
 END
 GO
 
-DELIMITER ;
+
+-- -----------------------------------------------------
+-- Stored Procedure `sp_getUser`
+-- -----------------------------------------------------
+DROP PROCEDURE IF EXISTS `sp_getUser`
+GO
+CREATE PROCEDURE sp_getUser(
+    IN s_user_id INT
+)
+BEGIN
+ 	SELECT id_user, username, first_name, last_name, email, password, salt
+ 	FROM users
+ 	WHERE id_user = s_user_id;
+END
+GO
+
+-- -----------------------------------------------------
+-- Stored Procedure `sp_delete_User`
+-- -----------------------------------------------------
+DROP PROCEDURE IF EXISTS `sp_delete_User`
+GO
+CREATE PROCEDURE sp_delete_User(
+	IN s_user_id INT
+)
+BEGIN
+	DELETE FROM users 
+	WHERE id_user = s_user_id;
+	DELETE FROM login_attempts 
+	WHERE id_user = s_user_id;
+END
+GO
+
+-- -----------------------------------------------------
+-- Stored Procedure `sp_saveUser`
+-- -----------------------------------------------------
+DROP PROCEDURE IF EXISTS `sp_saveUser`
+GO
+CREATE PROCEDURE sp_saveUser(
+	IN id_user INT(11),
+	IN username VARCHAR(30),
+	IN first_name VARCHAR(75),
+	IN last_name VARCHAR(50),
+	IN email VARCHAR(50),
+	IN password VARCHAR(128),
+	IN salt VARCHAR(128)
+)
+BEGIN
+	IF EXISTS (SELECT * FROM users WHERE id_user = s_id_user) THEN
+		UPDATE users SET
+			username = s_username,
+			first_name = s_first_name,
+			last_name = s_last_name,
+			email = s_email,
+			password = s_password,
+			salt = s_salt
+		WHERE id_user = s_id_user;
+	ELSE
+		INSERT INTO `users` (`username`, `first_name`, `last_name`, `email`, `password`, `salt`) 
+		VALUES (s_username, s_first_name, s_last_name, s_email, s_password, s_salt);
+	END IF;
+END
+GO
