f5fd0ff8539b7700146fa48b2102e4c8a8e75d00
Andrew <aassa013@uottawa.ca>
2013-11-06 21:25:37 -0500

    removed unnecessary old commented out code from my out-of-project implementation

--- Code/RestaurantManagementSoftware/application/views/login/process_login.php
+++ Code/RestaurantManagementSoftware/application/views/login/process_login.php
@@ -5,57 +5,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//function login($username, $password, $users) {
-//   // Using prepared Statements means that SQL injection is not possible. 
-//   if ($stmt = $users->prepare("SELECT id, email, password, salt FROM users WHERE username = ? LIMIT 1")) { 
-//      $stmt->bind_param('s', $username); // Bind "$username" to parameter.
-//      $stmt->execute(); // Execute the prepared query.
-//      $stmt->store_result();
-//      $stmt->bind_result($user_id, $email, $db_password, $salt); // get variables from result.
-//      $stmt->fetch();
-//      $password = hash('sha512', $password.$salt); // hash the password with the unique salt.
-// 
-//      if($stmt->num_rows == 1) { // If the user exists
-//         // We check if the account is locked from too many login attempts
-//         //if(checkbrute($user_id, $users) == true) { 
-//            // Account is locked
-//            // Send an email to user saying their account is locked
-//          //  return false;
-//         } else {
-//         if($db_password == $password) { // Check if the password in the database matches the password the user submitted. 
-//            // Password is correct!
-// 
-// 
-//               $user_browser = $_SERVER['HTTP_USER_AGENT']; // Get the user-agent string of the user.
-// 
-//               $user_id = preg_replace("/[^0-9]+/", "", $user_id); // XSS protection as we might print this value
-//               $_SESSION['user_id'] = $user_id; 
-//               $username = preg_replace("/[^a-zA-Z0-9_\-]+/", "", $username); // XSS protection as we might print this value
-//               $_SESSION['username'] = $username;
-//               $_SESSION['login_string'] = hash('sha512', $password.$user_browser);
-//               // Login successful.
-//               return true;    
-//         } else {
-//            // Password is not correct
-//            // We record this attempt in the database
-//            $now = time();
-//            $ipaddress = get_client_ip();
-//            $users->query("INSERT INTO `login_attempts` (id, timeOfAttempt, ip_address) VALUES ('$user_id', '$now', '$ipaddress'");
-//            return false;
-//         }
-//      }
-//      } else {
-//         // No user exists. 
-//         return false;
-//      }
-//   }
-//if (isset($user)) {
-//    foreach ($user as $u) {
-//        echo 'username: ' . $u->getUsername();
-//        echo 'salt: ' . $u->getSalt();
-//    }
-//}
-
 $user;
 if (isset($_POST['username'], $_POST['password'])) {
     $username = $_POST['username'];
