cf80f88a48b4ccb8e9ab8b36cf8415b56e594fbb
david8640 <david_8640@hotmail.com>
2013-11-08 18:30:52 -0500

    #64 Try to identify the problem with the form

--- Code/RestaurantManagementSoftware/application/views/index.php
+++ Code/RestaurantManagementSoftware/application/views/index.php
@@ -1,7 +1,7 @@
-<?php echo Html::anchor('login/register', 'Register!');?> <br/>
-<?php echo Html::anchor('login/login', 'Login');?> <br/>
-<?php echo Html::anchor('login/logout', 'Logout');?> <br/>
-<?php echo Html::anchor('supplier/findAll', 'Suppliers');?> <br/>
+<?php echo HTML::anchor('login/register', 'Register!');?> <br/>
+<?php echo HTML::anchor('login/login', 'Login');?> <br/>
+<?php echo HTML::anchor('login/logout', 'Logout');?> <br/>
+<?php echo HTML::anchor('supplier/findAll', 'Suppliers');?> <br/>
 <?php echo Form::button('test', 'Test'); ?>
 
 
