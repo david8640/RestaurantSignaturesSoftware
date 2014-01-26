cc5ce874fe066bc3078b221aa4abc62f99344b2c
david8640 <david_8640@hotmail.com>
2014-01-26 11:35:53 -0500

    #60 - Implementation of order controller step 1

--- Code/RestaurantManagementSoftware/application/classes/Controller/Order.php
+++ Code/RestaurantManagementSoftware/application/classes/Controller/Order.php
@@ -1,5 +1,4 @@
 <?php
-
 /* 
  *  <copyright file="Order.php" company="RestaurantManagementSoftware">
  *     Copyright (c) 2013, 2014 All Right Reserved
@@ -18,17 +17,60 @@ class Controller_Order extends Controller_Template_Generic {
         $orders = $repo->getAll();
         
         // Transfer the information to the view.
-        $view = View::factory('order/orderList')
+        $view = View::factory('order/orders')
                     ->set('orders', $orders);
         
-        $this->template->title = __('Order List');
+        $this->template->title = __('Orders');
         $this->template->content = $view;
     }
 
-     /**
+    /**
+     * Get all the orders for a restaurant.
+     */
+    public function action_getRestaurantOrders() {
+        // Get all the information from the repository.
+        $repo = new Repository_Order();
+        $orders = $repo->getRestaurantOrders();
+        
+        // Transfer the information to the view.
+        $view = View::factory('order/orders')
+                    ->set('orders', $orders);
+        
+        $this->template->title = __('Orders');
+        $this->template->content = $view;
+    }
+    
+    /**
+     * Step 1 of the order wizards.
+     */
+    public function action_getInfoStep1() {
+        // Get all the information from the repository.
+        $repo = new Repository_SupplierProduct();
+        $products = $repo->getProducts();
+        
+        // Transfer the information to the view.
+        $view = View::factory('order/step1')
+                    ->set('products', $products);
+        
+        $this->template->title = __('Place Order : Step 1');
+        $this->template->content = $view;
+    }
+
+    /**
+     * Get the check users in a specific format (userid1, userid2, ...)
+     */
+    /*private function getformattedCheckUsers($checkedValues) {
+        $formattedStr = '';
+        foreach($checkedValues as $value){
+            $formattedStr .= $value.',';
+        }
+        return $formattedStr;
+    }*/
+    
+    /**
      * Place an order.
      */
-    public function action_order() {
+    /*public function action_order() {
         // Get all the information from the repository.
         $repo = new Repository_Order();
         $orders = $repo->getProducts();
@@ -39,18 +81,7 @@ class Controller_Order extends Controller_Template_Generic {
         
         $this->template->title = __('Order List');
         $this->template->content = $view;
-    }
-
-    /**
-     * Get the check users in a specific format (userid1, userid2, ...)
-     */
-    private function getformattedCheckUsers($checkedValues) {
-        $formattedStr = '';
-        foreach($checkedValues as $value){
-            $formattedStr .= $value.',';
-        }
-        return $formattedStr;
-    }
+    }*/
 }
 
 ?>
