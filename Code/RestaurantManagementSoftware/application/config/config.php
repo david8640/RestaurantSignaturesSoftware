45ee3d6916ad9822ec16b543860926a917f0955a
david8640 <david_8640@hotmail.com>
2014-03-11 11:21:20 -0400

    Fix tests

--- Code/RestaurantManagementSoftware/application/config/config.php
+++ Code/RestaurantManagementSoftware/application/config/config.php
@@ -5,19 +5,19 @@ return array(
         'default' => '13'
     ),
     // LOCAL
-    'db' => array(
+    /*'db' => array(
         'Host' => 'localhost',
         'User' => 'root',
         'Password' => 'root',
         'Database' => 'restaurantManagementSoftware',
         'Port' => '8889'
-    )
+    )*/
     // Website
-    /*'db' => array(
+    'db' => array(
         'Host' => 'db498909085.db.1and1.com',
         'User' => 'dbo498909085',
         'Password' => 'davidfortin8640',
         'Database' => 'db498909085',
         'Port' => '3306'
-    )*/
+    )
 );
