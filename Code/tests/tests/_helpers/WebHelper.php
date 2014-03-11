846b552efb01d75fef945c2668dc187e08f11505
david8640 <david_8640@hotmail.com>
2014-03-10 20:50:52 -0400

    #109 Fix tests

--- Code/tests/tests/_helpers/WebHelper.php
+++ Code/tests/tests/_helpers/WebHelper.php
@@ -13,6 +13,13 @@ function logout(\WebGuy $I) {
     $I->click('Logout');
 }
 
+function getUrl() {
+    // Local
+    //return "/seg4910-project/Code/RestaurantManagementSoftware/index.php/";
+    // Server
+    return "/current/Code/RestaurantManagementSoftware/index.php/";
+}
+
 class WebHelper extends \Codeception\Module
 {
 }
