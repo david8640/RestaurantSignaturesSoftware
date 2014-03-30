ad265608104f47b8cc090a6b04b223cf9ea64612
Andrew <aassa013@uottawa.ca>
2014-03-30 16:17:57 -0400

    #108 Added quick links to the home page

--- Code/RestaurantManagementSoftware/application/views/index.php
+++ Code/RestaurantManagementSoftware/application/views/index.php
@@ -1,5 +1,13 @@
-<h2>Home Page</h2>
+<h2 align="center">Home Page</h2>
 
-
-
+<table  cellspacing=0 cellpadding=0 width="600" align="center">
+  <tr>
+    <td align="center" valign="top"><a href="/seg4910-project/Code/RestaurantManagementSoftware/index.php/order/getStep1/-1/findAll"><img src="/seg4910-project/Code/RestaurantManagementSoftware/application/style/images/new_order.png" /></a></td>
+    <td align="center" valign="top"><a href="/seg4910-project/Code/RestaurantManagementSoftware/index.php/order/findAll"><img src="/seg4910-project/Code/RestaurantManagementSoftware/application/style/images/view_orders.png" /></a></td>
+  </tr>
+  <tr>
+    <td align="center" valign="top"><a href="/seg4910-project/Code/RestaurantManagementSoftware/index.php/order/getStep1/-1/findAll">New Order</a></td>
+    <td align="center" valign="top"><a href="/seg4910-project/Code/RestaurantManagementSoftware/index.php/order/findAll">View Orders</a></td>
+  </tr>
+</table>
 
