c75dfc82f505b207cac91e7ae149b8ded45f4a00
Andrew <aassa013@uottawa.ca>
2014-03-30 23:58:57 -0400

    Fixed icon size mis-matches in quick-links on home page

--- Code/RestaurantManagementSoftware/application/views/index.php
+++ Code/RestaurantManagementSoftware/application/views/index.php
@@ -1,18 +1,36 @@
-<h2>Home Page</h2>
+<h2 align="center">Home Page</h2>
 
 <h4>Quick Links</h4>
 <div class="shortcut">
     <ul>
         <li>
             <a href="<?php echo URL::site('order/getStep1', array('id' => -1, 'lastAction' => 'findAll')); ?>">
-                <?php echo HTML::image('application/style/images/new_order.png'); ?>
+                <?php echo HTML::image('application/style/images/new-order.png'); ?>
                 <span>New Order</span>
             </a>
         </li>
         <li>
             <a href="<?php echo URL::site('order/findAll'); ?>">
-                <?php echo HTML::image('application/style/images/view_orders.png'); ?>
+                <?php echo HTML::image('application/style/images/view-orders.png'); ?>
                 <span>View Orders</span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo URL::site('order/getStep1', array('id' => -1, 'lastAction' => 'findAll')); ?>">
+                <?php echo HTML::image('application/style/images/inventory.png'); ?>
+                <span>Inventory</span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo URL::site('order/findAll'); ?>">
+                <?php echo HTML::image('application/style/images/suppliers.png'); ?>
+                <span>Supplier Info</span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo URL::site('order/findAll'); ?>">
+                <?php echo HTML::image('application/style/images/user-access.png'); ?>
+                <span>User Access</span>
             </a>
         </li>
     </ul>
